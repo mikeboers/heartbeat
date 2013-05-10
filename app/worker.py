417b6a4441a22b3418504815417155f5cfe3fd30
Mike Boers <github@mikeboers.com>
2013-05-09 20:30:16 -0700

    Worker will properly trigger sub-minute jobs

--- app/worker.py
+++ app/worker.py
@@ -17,8 +17,12 @@ log = logging.getLogger(__name__)
 
 def main():
 
-    next_time = int(time.time()) / 60 * 60
+    next_time = int(time.time() + 60) / 60 * 60
+    last_time = next_time - 60
+
     while True:
+
+        # Wait for the full minute to pass.
         now = time.time()
         to_wait = math.ceil(next_time - now)
         if to_wait > 0:
@@ -29,11 +33,15 @@ def main():
         for service in db.session.query(Service):
 
             if service.cron_spec and service.can_active_check:
-                cron = croniter(service.cron_spec, next_time - 1)
+
+                # See if the next time to run is within the bracket that
+                # just passed.
+                cron = croniter(service.cron_spec, last_time)
                 cron_next = cron.get_next()
-                if cron_next == next_time:
+                if cron_next <= next_time:
                     log.debug('checking "%s"...' % service.name)
                     service.active_check()
+
                 else:
                     log.debug('next check of "%s" in %ds' % (service.name, math.ceil(cron_next - now)))
 
